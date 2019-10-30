c955292768851524f3868daf7fd1abf0c659739b
Jelle Vermandere <jelle@MacBook-Pro-van-Jelle.local>
2019-10-30 19:47:43 +0100

    update

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,9 +1,9 @@
 <?php
 
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "re6zee6d";
-$dbName = "thesisdb";
+$dbServername = "us-cdbr-iron-east-05.cleardb.net";
+$dbUsername = "b0a325ab314c60";
+$dbPassword = "4c33dab3";
+$dbName = "heroku_50e6f23ea25384f";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 
