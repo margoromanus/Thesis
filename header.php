3fa99d15d354069cbf77e48bd086a9130514fd6b
Jelle Vermandere <jelle@MacBook-Pro-van-Jelle.local>
2019-11-12 20:26:30 +0100

    update

--- header.php
+++ header.php
@@ -15,7 +15,6 @@
         <link rel="stylesheet" href="styles.css">
         <script src="https://aframe.io/releases/0.9.2/aframe.min.js"></script>
         <script src="https://unpkg.com/aframe-orbit-controls@1.0.0/dist/aframe-orbit-controls.min.js"></script>
-        <script src="https://cdn.rawgit.com/jeromeetienne/AR.js/1.7.1/aframe/build/aframe-ar.js"></script> 
         <script src="includes/resize.js"></script>
        
     </head>
