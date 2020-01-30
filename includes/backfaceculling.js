d6b5f8818ebf26e65221a9a95e1180f582741034
Jelle Vermandere <jelle@MacBook-Pro-van-Jelle.local>
2020-01-30 09:59:20 +0100

    update

--- includes/backfaceculling.js
+++ includes/backfaceculling.js
@@ -6,9 +6,9 @@ AFRAME.registerComponent('backfaceculling', {
         const obj = this.el.getObject3D('mesh');
         // Go over the submeshes and modify materials we want.
         obj.traverse(node => {
-          if (node.name.indexOf('ship') !== -1) {
+
             node.doubleSided = false;
-          }
+          
         });
       });
     }
